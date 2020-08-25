d392b81d2f9aa3d36954f18f216ef4863a997ee1
Luther <luther.m.cordeiro@gmail.com>
2020-08-25 17:55:37 -0300

    Header Component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import './styles.css';
+
+import logo from '../../assets/images/logo.svg';
+import search from '../../assets/icons/search.svg';
+import shoppingBag from '../../assets/icons/shopping-bag.svg';
+import menuMobile from '../../assets/icons/menu-mobile.svg'
+
+function Header() {
+  return (
+    <div className="Header">
+      <div id="logo">
+        <img src={logo} alt="logotype"/>
+      </div>
+      
+      <header className="top-bar">
+       
+       <div className='left-item-top-bar'>
+         <img src={menuMobile} alt="Menu"/>
+         <p>Entrar | Cadastrar-se</p>
+       </div>
+
+      <div className="mid-item-top-bar">
+        <div id="mid-content">
+          <div>
+            <p>SAPATOS</p>
+          </div>
+          <div>
+            <p>BOLSAS</p>
+          </div>
+          
+          <div>
+            <p>ACESSÓRIOS</p>
+          </div>
+          
+          <div>
+            <p>OFF</p>
+          </div>
+             
+        </div>      
+      </div>
+
+       <div className="search-top-bar">
+            <img src={search} alt="search"/>
+            <input type="text" value='Buscar'/>
+       </div>
+       <div className="shopping-bag-top-bar">
+          <img src={shoppingBag} alt="shopping bag"/>
+          <span>0</span>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/pages/ShowProduct/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+import Header from '../../components/Header'
+
+function Index(){
+  return(
+    <>
+       <Header/>
+      <div>
+        home /sapatos
+      </div>
+    </>
+     
+    
+  )
+  
+}
+export default Index;
